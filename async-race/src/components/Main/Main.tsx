538a5d15d33f88f85eb60c19a8bd3e8ef3d349d6
grishanova-oa <grishanova.oa@gmail.com>
2023-01-23 10:36:09 +0100

    feat: add main section

--- async-race/src/components/Main/Main.tsx
+++ async-race/src/components/Main/Main.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { IGarage } from '../../types';
 import { GarageSection } from '../GarageSection';
 import './MainStyles.css';
 
-export const Main = () => {
+interface IMain {
+  isRaced: boolean;
+  garageData: IGarage[];
+  velocities: Record<number, number>;
+  brokenEngines: number[];
+}
+export const Main = ({ isRaced, garageData, velocities, brokenEngines }: IMain) => {
   return (
     <div className="main">
       <h3>Garage: (4)</h3>
       <h4>Page #1</h4>
-      <GarageSection />
+      {garageData.map((car) => (
+        <GarageSection
+          key={car.id}
+          car={car}
+          isRaced={isRaced}
+          velocity={velocities[car.id]}
+          isBroken={brokenEngines.includes(car.id)}
+        />
+      ))}
     </div>
   );
 };
